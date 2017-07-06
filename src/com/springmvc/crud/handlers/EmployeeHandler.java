69efeebd81dd5bbf8cfa5b97cb4c1db1b65e67ad
weiqy <weiqy@tfinfo.cn>
2017-07-06 23:23:45 +0800

    add crud

--- src/com/springmvc/crud/handlers/EmployeeHandler.java
+++ src/com/springmvc/crud/handlers/EmployeeHandler.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -20,6 +21,12 @@ public class EmployeeHandler {
 	@Autowired
 	private DepartmentDao departmentDao;
 
+	@RequestMapping(value="/emp/{id}",method=RequestMethod.DELETE)
+	public String delete(@PathVariable("id") Integer id){
+		employeeDao.delete(id);
+		return "redirect:/emps";
+	}
+	
 	@RequestMapping(value = "/emp", method = RequestMethod.POST)
 	public String save(Employee employee) {
 		employeeDao.save(employee);
@@ -29,7 +36,7 @@ public class EmployeeHandler {
 	@RequestMapping(value = "/emp", method = RequestMethod.GET)
 	public String input(Map<String, Object> map) {
 		map.put("departments", departmentDao.getDepartments());
-		map.put("employee", new Employee());
+		map.put("employee", new Employee()); //不需要表单回显，故新建个对象
 		return "input";
 	}
 
