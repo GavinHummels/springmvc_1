43e55bdb096a48339a3b23aaf89ae11d3998f85e
GavinHummels <1196529490@qq.com>
2017-07-07 17:37:29 +0800

    modify crud

--- src/com/springmvc/crud/handlers/EmployeeHandler.java
+++ src/com/springmvc/crud/handlers/EmployeeHandler.java
@@ -4,9 +4,11 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.springmvc.crud.dao.DepartmentDao;
 import com.springmvc.crud.dao.EmployeeDao;
@@ -21,12 +23,32 @@ public class EmployeeHandler {
 	@Autowired
 	private DepartmentDao departmentDao;
 
-	@RequestMapping(value="/emp/{id}",method=RequestMethod.DELETE)
-	public String delete(@PathVariable("id") Integer id){
+	@ModelAttribute
+	public void getEmployee(@RequestParam(value = "id", required = false) Integer id, Map<String, Object> map) {
+		if (id != null) {
+			map.put("employee", employeeDao.get(id));
+		}
+	}
+
+	@RequestMapping(value = "/emp", method = RequestMethod.PUT)
+	public String update(Employee employee) {
+		employeeDao.save(employee);
+		return "redirect:/emps";
+	}
+
+	@RequestMapping(value = "emp/{id}", method = RequestMethod.GET)
+	public String input(@PathVariable(value = "id") Integer id, Map<String, Object> map) {
+		map.put("employee", employeeDao.get(id));
+		map.put("departments", departmentDao.getDepartments());
+		return "input";
+	}
+
+	@RequestMapping(value = "/emp/{id}", method = RequestMethod.DELETE)
+	public String delete(@PathVariable("id") Integer id) {
 		employeeDao.delete(id);
 		return "redirect:/emps";
 	}
-	
+
 	@RequestMapping(value = "/emp", method = RequestMethod.POST)
 	public String save(Employee employee) {
 		employeeDao.save(employee);
@@ -36,7 +58,11 @@ public class EmployeeHandler {
 	@RequestMapping(value = "/emp", method = RequestMethod.GET)
 	public String input(Map<String, Object> map) {
 		map.put("departments", departmentDao.getDepartments());
-		map.put("employee", new Employee()); //不需要表单回显，故新建个对象
+		map.put("employee", new Employee()); // 不需要表单回显，故新建个对象
+//		Map<String,String> genders = new HashMap<>();
+//		genders.put("1","Male");
+//		genders.put("0","Female");
+//		map.put("genders", genders);
 		return "input";
 	}
 
--- webapps/WEB-INF/views/input.jsp
+++ webapps/WEB-INF/views/input.jsp
@@ -3,6 +3,7 @@
 <%@page import="java.util.HashMap"%>
 <%@page import="java.util.Map"%>
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
@@ -11,15 +12,35 @@
 <title>Insert title here</title>
 </head>
 <body>
+
+	<form action="testConversionServiceConverer" method="post">
+		<!-- lastname-email-gender-department.id 例如：GG-gg@qq.com-0-105 -->
+		Employee: <input type="text" name="employee">
+	</form>
+	<br><br>
 	<!-- 
 		1. 为什么使用 form 标签
 		可以更快速地开发出表单页面，而且可以更方便地进行表单值的回显
 		2. 可以通过 modelAttribute 属性指定绑定的模型属性，若没有指定该属性，则默认从 request 域对象中读取
 		command 的表单 bean，如果该属性值也不存在，则会发生错误。
 	 -->
-	<form:form action="emp" method="POST" modelAttribute="employee">
+	 <!-- 
+	 	这里的 action 使用绝对路径，${pageContext.request.contextPath}，后面所有的路径都建议要绝对路径
+	  -->
+	<form:form action="${pageContext.request.contextPath }/emp" method="POST" modelAttribute="employee">
 		<!-- path 属性对应 html 表单标签的 name 属性值 -->
-	 	LastName: <form:input path="lastName" />
+	 	<c:if test="${employee.id == null}">
+		 	LastName: <form:input path="lastName" />
+	 	</c:if>
+		<c:if test="${employee.id != null}">
+			<form:hidden path="id"/>
+			<%-- 
+				对于 _method 不能使用 form:hidden 标签，因为 modelAttribute 对应的 bean 中没有 _method 这个属性
+				<form:hidden path="_method" value="PUT"/>
+			 --%>
+			<input type="hidden" name="_method" value="PUT">
+		</c:if>
+			 	
 		<br><br>
 	 	Email: <form:input path="email" />
 		<br><br>
@@ -34,6 +55,15 @@
 	 	Department: <form:select path="department.id" items="${departments }" 
 	 	itemLabel="departmentName" itemValue="id"></form:select>
 	 	<br><br>
+	 	<!-- 
+	 		1. 数据类型转换
+	 		2. 数据类型格式化
+	 		3. 数据校验
+	 	 -->
+	 	 <!-- 
+	 	 Birth: <form:input path="birth"/>
+	 	<br><br>
+	 	  -->
 	 	<input type="submit" value="Submit">
 	</form:form>
 
